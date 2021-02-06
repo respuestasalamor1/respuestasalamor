8fb08fa3e3a63401a875a5c1c345bbd596b2d5bf
respuestasalamor1 <respuestasalamor1@gmail.com>
2021-02-06 18:09:00 -0500

    Ajustes para mail to en contactenos.component

--- src/app/modules/contactenos/contactenos.component.ts
+++ src/app/modules/contactenos/contactenos.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactenos.component.scss']
 })
 export class ContactenosComponent implements OnInit {
+  nombre?: string;
+  correo?: string;
+  telefono?: string;
+  mensaje?: string;
+  emailEmpresa?: string;
+  asunto?: string;
+  mensajeAEnviar?: string;
+  constructor() {
+  }
 
-  constructor() { }
 
   ngOnInit(): void {
+  }
+
+  enviarEmail() {
+    this.emailEmpresa = 'respuestasalamor1@gmail.com';
+    this.asunto = 'Consulta%20con%20el%20maestro';
+    this.mensajeAEnviar = 'Hola, soy ' + this.nombre + ', quisiera consultar con el maestro, mi numero telefonico es ' + this.telefono + ' y mi correo electronico es ' + this.correo;
+    const mailTo = 'mailto:' + this.emailEmpresa + '?subject=' + this.asunto + '&body=' + this.mensajeAEnviar;
+    window.open(mailTo);
   }
 
 }
