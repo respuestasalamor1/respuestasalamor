f0b8b550be81b542cfc4026cf9707da5c3bb04f4
Yesid Cadavid Carvalho <yesid102009@hotmail.es>
2021-04-27 22:15:50 -0500

    Ajustes de politicas de privacidad

--- src/app/layout/chat/chat.component.ts
+++ src/app/layout/chat/chat.component.ts
@@ -5,10 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './chat.component.html'
 })
 export class ChatComponent implements OnInit {
-
+  validatorPolicy = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.validatorPolicy = false;
   }
 
+  validatorPol() {
+    if (this.validatorPolicy) {
+      this.validatorPolicy = false;
+    } else
+      this.validatorPolicy = true;
+  }
 }
